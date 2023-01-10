9958934b94742bcd60381c774063aa1974bfba25
Ola Fredrik Ansnes Kveli <ola.fredrik.kveli@itera.com>
2023-01-10 16:49:18 +0100

    Update to add cosmos db

--- FootballPredictionAPI/Program.cs
+++ FootballPredictionAPI/Program.cs
@@ -4,13 +4,19 @@ using System.Reflection.Metadata;
 using AutoMapper;
 using FootballPredictionAPI;
 using FootballPredictionAPI.Context;
+using Microsoft.Azure.Cosmos;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+var dbName = "FootballTeams";
 builder.Services.AddControllers();
-builder.Services.AddDbContext<FootballTeamContext>(opt => opt.UseInMemoryDatabase("FootballTeams"));
+using CosmosClient client = new(
+    accountEndpoint: builder.Configuration.GetConnectionString("COSMOS_ENDPOINT")!, 
+    authKeyOrResourceToken: builder.Configuration.GetConnectionString("COSMOS_KEY")!
+    );
+//builder.Services.AddDbContext<FootballTeamContext>(optionsAction => optionsAction.UseCosmos(connection, dbName));
+builder.Services.AddDbContext<FootballTeamContext>(opt => opt.UseInMemoryDatabase(dbName));
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
