a2103bda3a87554a506599fd7f1d7acaa5eccaa6
Ola Fredrik Ansnes Kveli <ola.fredrik.kveli@itera.com>
2023-01-26 14:42:43 +0100

    Add tests to project, as well as mocks.

---
+++ FootballPredictionAPITests/Controllers/FootballTeamsControllerTests.cs
@@ -0,0 +1,88 @@
+﻿using AutoFixture;
+using FootballPredictionAPI.Interfaces;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System.Diagnostics;
+
+namespace FootballPredictionAPI.Controllers.Tests
+{
+    [TestClass()]
+    public class FootballTeamsControllerTests
+    {
+        private readonly Mock<IFootballRepository>? _repository;
+        private readonly Fixture? _fixture;
+
+        public FootballTeamsControllerTests()
+        {
+            _fixture = new Fixture();
+            _repository = new Mock<IFootballRepository>();
+        ***REMOVED***
+
+        [TestInitialize]
+        public void Init()
+        {
+            Debug.WriteLine("Controller tests started...");
+        ***REMOVED***
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            Debug.WriteLine("Controller tests terminated...");
+        ***REMOVED***
+
+        [TestMethod("Test football teams controller")]
+        public void FootballTeamsControllerTest()
+        {
+            Assert.Fail();
+        ***REMOVED***
+
+        [Obsolete("This is no longer in use")]
+        [TestMethod("Test seed football controller")]
+        public void SeedFootballTeamTest()
+        {
+            Assert.Fail();
+        ***REMOVED***
+
+        [TestMethod("Test get teams controller")]
+        public void GetTeamsTest()
+        {
+            Assert.Fail();
+        ***REMOVED***
+
+        [TestMethod("Test get football team by id controller")]
+        public void GetFootballTeamTest()
+        {
+            Assert.Fail();
+        ***REMOVED***
+
+        [TestMethod("Test get football team by name controller")]
+        public void GetFootballTeamByNameTest()
+        {
+            Assert.Fail();
+        ***REMOVED***
+
+        [TestMethod("Test update football team controller")]
+        public void PutFootballTeamTest()
+        {
+            Assert.Fail();
+        ***REMOVED***
+
+        [TestMethod("Test post football team controller")]
+        public void PostFootballTeamTest()
+        {
+            Assert.Fail();
+        ***REMOVED***
+
+        [TestMethod("Test delete football team by id controller")]
+        public void DeleteFootballTeamTest()
+        {
+            Assert.Fail();
+        ***REMOVED***
+
+        [TestMethod("Test delete football team by name controller")]
+        public void DeleteFootballTeamByNameTest()
+        {
+            Assert.Fail();
+        ***REMOVED***
+    ***REMOVED***
+***REMOVED***
---
+++ FootballPredictionAPITests/Repositories/FootballRepositoryTests.cs
@@ -0,0 +1,169 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FootballPredictionAPI.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FootballPredictionAPI.Interfaces;
+using AutoMapper;
+using FootballPredictionAPI.Context;
+using Microsoft.Extensions.Configuration;
+using FootballPredictionAPI.Models;
+using System.Diagnostics;
+using Moq;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using FootballPredictionAPI.DTOs;
+
+namespace FootballPredictionAPI.Repositories.Tests
+{
+    [TestClass()]
+    public class FootballRepositoryTests : IFootballRepository
+    {
+        private FootballTeam? footballTeam;
+        private FootballRepository? _repository;
+
+        [TestInitialize]
+        public void Init()
+        {
+            Debug.WriteLine("Repository tests initialized..");
+            footballTeam = new()
+            {
+                Id = "Test-01",
+                Name = "Test",
+                Description = "Test 01 team",
+                MatchesWon = 3,
+                MatchesLost = 2,
+                MatchesDraw = 1
+            ***REMOVED***;
+        ***REMOVED***
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            Debug.WriteLine("Repository tests terminated..");
+        ***REMOVED***
+
+        [TestMethod("Test repository")]
+        public void FootballRepositoryTest()
+        {
+            Assert.Fail();
+        ***REMOVED***
+
+        [TestMethod("Get football teams")]
+        public void GetFootballTeamsTest()
+        {
+            Assert.Fail();
+        ***REMOVED***
+
+        [TestMethod("Get football team by id")]
+        public void GetFootballTeamByIdTest()
+        {
+            Assert.Fail();
+        ***REMOVED***
+
+        [TestMethod("Get football team by name")]
+        public void GetFootballTeamByNameTest()
+        {
+            Assert.Fail();
+        ***REMOVED***
+
+        [TestMethod("Update football team")]
+        public void UpdateFootballTeamTest()
+        {
+            Assert.Fail();
+        ***REMOVED***
+
+        [TestMethod("Add a football team")]
+        public void AddFootballTeamTest()
+        {
+            Assert.Fail();
+        ***REMOVED***
+
+        [TestMethod("Delete a football team by id")]
+        public void DeleteFootballTeamByIdTest()
+        {
+            Assert.Fail();
+        ***REMOVED***
+
+        [TestMethod("Delete a football team by name")]
+        public void DeleteFootballTeamByNameTest()
+        {
+            Assert.Fail();
+        ***REMOVED***
+
+        [Obsolete("No longer in use")]
+        [TestMethod("Seed data test")]
+        public void SeedTest()
+        {
+            Assert.Fail();
+        ***REMOVED***
+
+        [TestMethod("Calculate points for a football team")]
+        [Priority(9)]
+        [TestCategory("Internal logic")]
+        public void CalculatePointsTest()
+        {
+            var calculatedScore = _repository!.CalculatePoints(footballTeam!);
+            Assert.AreEqual(10, calculatedScore);
+        ***REMOVED***
+
+        [Obsolete("No longer in use")]
+        [TestMethod("Check if a football team table exists")]
+        public void FootballTeamTableExistsTest()
+        {
+            Assert.Fail();
+        ***REMOVED***
+
+        public Task<IEnumerable<FootballTeamDTO?>> GetFootballTeams()
+        {
+            throw new NotImplementedException();
+        ***REMOVED***
+
+        public Task<FootballTeamDTO?> GetFootballTeamById(string id)
+        {
+            throw new NotImplementedException();
+        ***REMOVED***
+
+        public Task<FootballTeamDTO?> GetFootballTeamByName(string name)
+        {
+            throw new NotImplementedException();
+        ***REMOVED***
+
+        public Task<FootballTeam?> UpdateFootballTeam(string id, FootballTeam footballTeamDto)
+        {
+            throw new NotImplementedException();
+        ***REMOVED***
+
+        public Task<FootballTeam?> AddFootballTeam(FootballTeamDTO footballTeam)
+        {
+            throw new NotImplementedException();
+        ***REMOVED***
+
+        public Task<FootballTeam?> DeleteFootballTeamById(string id)
+        {
+            throw new NotImplementedException();
+        ***REMOVED***
+
+        public Task<FootballTeam?> DeleteFootballTeamByName(string name)
+        {
+            throw new NotImplementedException();
+        ***REMOVED***
+
+        public Task<IEnumerable<FootballTeamDTO>> Seed()
+        {
+            throw new NotImplementedException();
+        ***REMOVED***
+
+        public int CalculatePoints(FootballTeam footballTeam)
+        {
+            throw new NotImplementedException();
+        ***REMOVED***
+
+        public bool FootballTeamTableExists()
+        {
+            throw new NotImplementedException();
+        ***REMOVED***
+    ***REMOVED***
+***REMOVED***
