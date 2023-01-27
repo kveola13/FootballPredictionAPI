4475383571f733b819c8a5631e892d837f0724b1
Ola Fredrik Ansnes Kveli <ola.fredrik.kveli@itera.com>
2023-01-27 09:58:28 +0100

    Fix to tests

--- FootballPredictionAPITests/Controllers/FootballTeamsControllerTests.cs
+++ FootballPredictionAPITests/Controllers/FootballTeamsControllerTests.cs
@@ -1,12 +1,6 @@
-﻿using AutoFixture;
-using FootballPredictionAPI.DTOs;
-using FootballPredictionAPI.Interfaces;
-using FootballPredictionAPI.Models;
-using Microsoft.AspNetCore.Http.HttpResults;
-using Microsoft.AspNetCore.Mvc;
+﻿using FootballPredictionAPI.Interfaces;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
-using NuGet.Protocol;
 using System.Diagnostics;
 
 namespace FootballPredictionAPI.Controllers.Tests
@@ -46,11 +40,7 @@ namespace FootballPredictionAPI.Controllers.Tests
         [TestMethod("Test get teams controller")]
         public void GetTeamsTest()
         {
-            var teamList = new List<FootballTeamDTO>() {
-                new FootballTeamDTO(),
-                new FootballTeamDTO()
-            ***REMOVED***;
-            Assert.IsNotNull(teamList);
+            throw new NotImplementedException();
         ***REMOVED***
 
         [TestMethod("Test get football team by id controller")]
@@ -62,7 +52,7 @@ namespace FootballPredictionAPI.Controllers.Tests
         [TestMethod("Test get football team by name controller")]
         public void GetFootballTeamByNameTest()
         {
-            throw new NotImplementedException()
+            throw new NotImplementedException();
         ***REMOVED***
 
         [TestMethod("Test update football team controller")]
--- FootballPredictionAPITests/Repositories/FootballRepositoryTests.cs
+++ FootballPredictionAPITests/Repositories/FootballRepositoryTests.cs
@@ -1,22 +1,9 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using FootballPredictionAPI.Repositories;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using FootballPredictionAPI.Interfaces;
-using AutoMapper;
-using FootballPredictionAPI.Context;
-using Microsoft.Extensions.Configuration;
 using FootballPredictionAPI.Models;
 using System.Diagnostics;
 using Moq;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.EntityFrameworkCore;
 using FootballPredictionAPI.DTOs;
-using FootballPredictionAPI.Controllers;
-using Microsoft.AspNetCore.Mvc;
 
 namespace FootballPredictionAPI.Repositories.Tests
 {
@@ -26,6 +13,7 @@ namespace FootballPredictionAPI.Repositories.Tests
         private readonly Mock<IFootballRepository>? _repository;
         private readonly FootballRepository? _footballRepository;
         private FootballTeam? footballTeam;
+        private FootballTeamDTO? footballTeamDTO;
 
         public FootballRepositoryTests()
         {
@@ -40,11 +28,19 @@ namespace FootballPredictionAPI.Repositories.Tests
             footballTeam = new()
             {
                 Id = "Test-01",
-                Name = "Test",
+                Name = "Test name",
                 Description = "Test 01 team",
                 MatchesWon = 3,
                 MatchesLost = 2,
                 MatchesDraw = 1
+            ***REMOVED***;
+            footballTeamDTO = new()
+            {
+                Name = "Test-DTO",
+                Description = "Test 02 team",
+                MatchesWon = 4,
+                MatchesLost = 5,
+                MatchesDraw = 6
             ***REMOVED***;
         ***REMOVED***
 
@@ -64,101 +60,131 @@ namespace FootballPredictionAPI.Repositories.Tests
             _repository!.Setup<Task<IEnumerable<FootballTeamDTO>>>(
                 rep => rep.GetFootballTeams()!
             ).Returns(Task.FromResult<IEnumerable<FootballTeamDTO>>(teamList));
-            var controller = new FootballTeamsController(null!, _repository.Object);
-            var result = controller.GetTeams().Result.Result;
-            var objectResultForStatusCode = (OkObjectResult)result!;
+            var result = _repository.Object.GetFootballTeams().Result;
 
-            Assert.AreEqual(200, objectResultForStatusCode.StatusCode);
+            Assert.AreEqual(teamList, result);
+            Assert.AreEqual(2, teamList.Count);
         ***REMOVED***
 
         [TestMethod("Get football team by id")]
         public void GetFootballTeamByIdTest()
         {
-            var team = new FootballTeamDTO();
 
             _repository!.Setup<Task<FootballTeamDTO>>(
-                rep => rep.GetFootballTeamById("testing")!
-            ).Returns(Task.FromResult<FootballTeamDTO>(team));
-            var controller = new FootballTeamsController(null!, _repository.Object);
-            var result = controller.GetTeams().Result.Result;
-            var objectResultForStatusCode = (OkObjectResult)result!;
+                rep => rep.GetFootballTeamById("Test-01")!
+            ).Returns(Task.FromResult(footballTeamDTO!));
 
-            Assert.AreEqual(200, objectResultForStatusCode.StatusCode);
+            var result = _repository.Object.GetFootballTeamById("Test-01").Result;
+
+            Assert.AreEqual(footballTeamDTO, result);
         ***REMOVED***
 
         [TestMethod("Get football team by name")]
         public void GetFootballTeamByNameTest()
         {
-            var team = new FootballTeamDTO();
-
             _repository!.Setup<Task<FootballTeamDTO>>(
-                rep => rep.GetFootballTeamByName("testing")!
-            ).Returns(Task.FromResult<FootballTeamDTO>(team));
-            var controller = new FootballTeamsController(null!, _repository.Object);
-            var result = controller.GetTeams().Result.Result;
-            var objectResultForStatusCode = (OkObjectResult)result!;
+                rep => rep.GetFootballTeamByName("Test-DTO")!
+            ).Returns(Task.FromResult(footballTeamDTO!));
+            var result = _repository.Object.GetFootballTeamByName("Test-DTO").Result;
 
-            Assert.AreEqual(200, objectResultForStatusCode.StatusCode);
+            Assert.AreEqual(footballTeamDTO, result);
         ***REMOVED***
 
         [TestMethod("Update football team")]
         public void UpdateFootballTeamTest()
         {
-            var team = new FootballTeam();
+            var updatedTeam = new FootballTeam()
+            {
+                Id = "new Test",
+                Name = "new test",
+                Description = "new test",
+                MatchesWon = 2,
+                MatchesDraw = 2,
+                MatchesLost = 2,
+            ***REMOVED***;
 
             _repository!.Setup<Task<FootballTeam>>(
-                rep => rep.UpdateFootballTeam("test", new FootballTeam())!
-            ).Returns(Task.FromResult<FootballTeam>(team));
-            var controller = new FootballTeamsController(null!, _repository.Object);
-            var result = controller.GetTeams().Result.Result;
-            var objectResultForStatusCode = (OkObjectResult)result!;
+                rep => rep.UpdateFootballTeam("test", updatedTeam)!
+            ).Returns(Task.FromResult(updatedTeam));
 
-            Assert.AreEqual(200, objectResultForStatusCode.StatusCode);
+            Assert.AreNotEqual(updatedTeam, footballTeam);
+            
+            var result = _repository.Object.UpdateFootballTeam("test", updatedTeam).Result;
+
+            Assert.AreEqual(updatedTeam, result);
         ***REMOVED***
 
         [TestMethod("Add a football team")]
         public void AddFootballTeamTest()
         {
-            var team = new FootballTeamDTO();
+            var teamToAdd = new FootballTeamDTO()
+            {
+                Name = "add test",
+                Description = "add test",
+                MatchesWon = 2,
+                MatchesDraw = 2,
+                MatchesLost = 2,
+            ***REMOVED***;
+            var teamToReturn = new FootballTeam()
+            {
+                Id="Add test id",
+                Name = "add test",
+                Description = "add test",
+                MatchesWon = 2,
+                MatchesDraw = 2,
+                MatchesLost = 2,
+            ***REMOVED***;
 
             _repository!.Setup<Task<FootballTeam>>(
-                rep => rep.AddFootballTeam(team)!
-            ).Returns(Task.FromResult<FootballTeam>(new FootballTeam()));
-            var controller = new FootballTeamsController(null!, _repository.Object);
-            var result = controller.GetTeams().Result.Result;
-            var objectResultForStatusCode = (OkObjectResult)result!;
+                rep => rep.AddFootballTeam(teamToAdd)!
+            ).Returns(Task.FromResult(teamToReturn));
+            var result = _repository.Object.AddFootballTeam(teamToAdd!).Result;
 
-            Assert.AreEqual(200, objectResultForStatusCode.StatusCode);
+            Assert.AreEqual(result, teamToReturn);
+            Assert.AreNotEqual(teamToAdd, teamToReturn);
+            Assert.AreNotSame(teamToAdd, teamToReturn);
+            Assert.AreSame(result, teamToReturn);
         ***REMOVED***
 
         [TestMethod("Delete a football team by id")]
         public void DeleteFootballTeamByIdTest()
         {
-            var team = new FootballTeamDTO();
-
+            Assert.IsNotNull(footballTeam);
             _repository!.Setup<Task<FootballTeam>>(
-                rep => rep.DeleteFootballTeamById("testing")!
-            ).Returns(Task.FromResult<FootballTeam>(new FootballTeam()));
-            var controller = new FootballTeamsController(null!, _repository.Object);
-            var result = controller.GetTeams().Result.Result;
-            var objectResultForStatusCode = (OkObjectResult)result!;
+                rep => rep.DeleteFootballTeamById("Test-01")!
+            ).Returns(Task.FromResult(footballTeam!));
 
-            Assert.AreEqual(200, objectResultForStatusCode.StatusCode);
+            FootballTeamDTO dto = null!;
+
+            _repository!.Setup<Task<FootballTeamDTO>>(
+                rep => rep.GetFootballTeamById("Test-01")!
+            ).Returns(Task.FromResult(dto!));
+
+            var result = _repository.Object.DeleteFootballTeamById("Test-01").Result;
+            var getTeam = _repository.Object.GetFootballTeamById("Test-01").Result;
+
+            Assert.AreEqual(result, footballTeam);
+            Assert.IsNull(getTeam);
         ***REMOVED***
 
         [TestMethod("Delete a football team by name")]
         public void DeleteFootballTeamByNameTest()
         {
-            var team = new FootballTeamDTO();
-
             _repository!.Setup<Task<FootballTeam>>(
-                rep => rep.DeleteFootballTeamByName("testing")!
-            ).Returns(Task.FromResult<FootballTeam>(new FootballTeam()));
-            var controller = new FootballTeamsController(null!, _repository.Object);
-            var result = controller.GetTeams().Result.Result;
-            var objectResultForStatusCode = (OkObjectResult)result!;
+                rep => rep.DeleteFootballTeamByName("Test-DTO")!
+            ).Returns(Task.FromResult(footballTeam!));
+            var result = _repository.Object.DeleteFootballTeamByName("Test-DTO").Result;
 
-            Assert.AreEqual(200, objectResultForStatusCode.StatusCode);
+            FootballTeamDTO nullValueDTO = null!;
+
+            _repository!.Setup<Task<FootballTeamDTO>>(
+                rep => rep.GetFootballTeamByName("Test-DTO")!
+            ).Returns(Task.FromResult(nullValueDTO!));
+
+            var getTeam = _repository.Object.GetFootballTeamByName("Test-DTO").Result;
+
+            Assert.AreEqual(result, footballTeam);
+            Assert.IsNull(getTeam);
         ***REMOVED***
 
         [Obsolete("No longer in use")]
