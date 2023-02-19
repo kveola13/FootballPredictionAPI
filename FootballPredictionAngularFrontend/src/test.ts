1129e25d783f714d9411c199198ca52a1d4a1bd0
Ola Fredrik Ansnes Kveli <olakveli@gmail.com>
2023-02-20 00:37:15 +0100

    update ng

--- FootballPredictionAngularFrontend/src/test.ts
+++ FootballPredictionAngularFrontend/src/test.ts
@@ -17,7 +17,9 @@ declare const require: {
 // First, initialize the Angular testing environment.
 getTestBed().initTestEnvironment(
   BrowserDynamicTestingModule,
-  platformBrowserDynamicTesting()
+  platformBrowserDynamicTesting(), {
+    teardown: { destroyAfterEach: false ***REMOVED***
+***REMOVED***
 );
 // Then we find all the tests.
 const context = require.context('./', true, /\.spec\.ts$/);
