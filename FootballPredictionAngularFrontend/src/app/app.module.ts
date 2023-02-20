9e86815936d490e9cd7bba70812aacf6e3d07a62
Ola Fredrik Ansnes Kveli <olakveli@gmail.com>
2023-02-20 08:06:49 +0100

    added new components for navigation

--- FootballPredictionAngularFrontend/src/app/app.module.ts
+++ FootballPredictionAngularFrontend/src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { IndexComponent } from './index/index.component';
+import { AboutComponent } from './about/about.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    IndexComponent,
+    AboutComponent,
+    PagenotfoundComponent
   ],
   imports: [
     BrowserModule,
