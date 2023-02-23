d50115d495945870ef7ab34dd2f6de07f495eee3
Ola Fredrik Ansnes Kveli <olakveli@gmail.com>
2023-02-23 15:08:04 +0100

    fix tests

--- footballprediction-react-frontend/src/App.test.js
+++ footballprediction-react-frontend/src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/App is running/i);
   expect(linkElement).toBeInTheDocument();
 ***REMOVED***);
